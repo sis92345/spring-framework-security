936d9cbf32135eb9570c3894d30d04993eaf5bd4
Ted <anjfqhk1014@naver.com>
2023-05-07 21:18:51 +0900

    [build][공통] : Annotation 기반 Filter 등록을 위해 ServletComponentScan annotation 추가

--- src/main/java/com/cos/security/SpringSecurityApplication.java
+++ src/main/java/com/cos/security/SpringSecurityApplication.java
@@ -2,11 +2,13 @@ package com.cos.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * Spring Security Sample
  * */
 @SpringBootApplication
+@ServletComponentScan
 public class SpringSecurityApplication {
 		
 		public static void main( String[] args ) {
